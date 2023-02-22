8384f1d269de75a8036d4b04fcd37e65311a8ad7
Ashish Gaikwad <ashish.gaikwad@voidstarindia.com>
2023-02-22 16:38:50 +0530

    Complate form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  @ViewChild('f') signupFomr: NgForm;
+  defaultString = 'Your pet';
+  comment = '';
+  genders = ['Male', 'Female', 'other'];
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupFomr.setValue({
+    //   userData: {
+    //     userName: suggestedName,
+    //   },
+    //   secret:''
+    // });
+
+    this.signupFomr.form.patchValue({
+      userData: {
+        username: suggestedName,
+      },
+    });
+  }
+
+  //   onSubmit(form: NgForm) {
+  //     console.log('Form is working', form);
+  //   }
+
+  onSubmit() {
+    console.log(this.signupFomr, 'signupForm....');
   }
 }
